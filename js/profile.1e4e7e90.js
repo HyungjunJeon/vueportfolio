561b0852dda520e0a9e76e2c204f44258e98b0c3
root <root@-X7967T.localdomain>
2021-12-18 23:31:24 +0900

    Make profile component

--- js/profile.1e4e7e90.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkvueportfolio"]=self["webpackChunkvueportfolio"]||[]).push([[845],{875:function(n,e,o){o.r(e),o.d(e,{default:function(){return a}});var r=o(252);const u=(0,r._)("h1",null,"This is an Profile page",-1),t=[u];function i(n,e,o,u,i,l){return(0,r.wg)(),(0,r.iD)("div",null,t)}var l={name:"Profile",components:{}},f=o(744);const s=(0,f.Z)(l,[["render",i]]);var a=s}}]);
-//# sourceMappingURL=profile.1e4e7e90.js.map
